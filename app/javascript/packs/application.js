1140d05923e060b799955f9837e2a9ffc4fe933c
Hamzat Ajanaku <hamzatajanaku@gmail.com>
2022-04-26 12:07:18 +0100

    initial commit

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,42 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import "bootstrap"
+import "jquery"
+import 'owl.carousel/dist/assets/owl.carousel.min.css';
+import 'owl.carousel';
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+import "controllers"
+
+$(document).on("turbolinks:load", function () {
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+  })
+})
